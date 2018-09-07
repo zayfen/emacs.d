c97ce442c813163f837762c3c65b5517edb23792
zhangyunfeng <zhangyunfeng0101@gmail.com>
2018-09-07 15:20:11 +0800

    use eglot

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -1,3 +1,5 @@
+(require 'eglot)
+
 (setq auto-mode-alist
       (append '(("SConstruct\\'" . python-mode)
                 ("SConscript\\'" . python-mode))
@@ -5,19 +7,23 @@
 
 (require-package 'pip-requirements)
 
-(when (maybe-require-package 'anaconda-mode)
+(when (maybe-require-package 'eglot)
   (after-load 'python
-    (add-hook 'python-mode-hook 'anaconda-mode)
-    (add-hook 'python-mode-hook 'anaconda-eldoc-mode))
-  (when (maybe-require-package 'company-anaconda)
-    (after-load 'company
-      (add-hook 'python-mode-hook
-                (lambda () (sanityinc/local-push-company-backend 'company-anaconda))))))
+    ;;(add-hook 'python-mode-hook 'anaconda-mode)
+    (add-hook 'python-mode-hook 'eglot-ensure)
+		(flymake-mode nil)
+    ;; (add-hook 'python-mode-hook 'anaconda-eldoc-mode))
+		))
+
 
 (add-hook 'python-mode-hook
-          (lambda ()
-            (setq-default indent-tabs-mode t)
-            (setq-default tab-width 2)
-            (setq-default python-indent-offset 2)
-            (add-to-list 'write-file-functions 'delete-trailing-whitespace)))
+          (lambda () (sanityinc/local-push-company-backend 'company-anaconda)
+						(setq-default indent-tabs-mode t)
+						(setq-default tab-width 2)
+						(setq-default python-indent-offset 2)
+						(add-to-list 'write-file-functions 'delete-trailing-whitespace)
+						(flymake-mode nil)
+						))
+
+
 (provide 'init-python)
