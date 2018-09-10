71aa3cf5aca903cf479b9ccb4ac3f1541a144a03
zhangyunfeng <zhangyunfeng0101@gmail.com>
2018-09-10 11:16:56 +0800

    change files'

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -7,17 +7,22 @@
 
 (require-package 'pip-requirements)
 
-(when (maybe-require-package 'eglot)
-  (after-load 'python
-    ;;(add-hook 'python-mode-hook 'anaconda-mode)
-    (add-hook 'python-mode-hook 'eglot-ensure)
-		(flymake-mode nil)
-    ;; (add-hook 'python-mode-hook 'anaconda-eldoc-mode))
-		))
+;; (when (maybe-require-package 'eglot)
+;;   (after-load 'python
+;;     ;;(add-hook 'python-mode-hook 'anaconda-mode)
+;;     (add-hook 'python-mode-hook 'eglot-ensure)
+;; 		(flymake-mode nil)
+;;     ;; (add-hook 'python-mode-hook 'anaconda-eldoc-mode))
+;; 		))
 
+(when (maybe-require-package 'anaconda-mode)
+  (after-load 'python
+		(add-hook 'python-mode-hook 'anaconda-mode)
+    (add-hook 'python-mode-hook 'anaconda-eldoc-mode)))
 
 (add-hook 'python-mode-hook
-          (lambda () (sanityinc/local-push-company-backend 'company-anaconda)
+          (lambda ()
+						;; (sanityinc/local-push-company-backend 'company-anaconda)
 						(setq-default indent-tabs-mode t)
 						(setq-default tab-width 2)
 						(setq-default python-indent-offset 2)
@@ -25,5 +30,7 @@
 						(flymake-mode nil)
 						))
 
+(setq anaconda-mode-localhost-address "localhost")
+(setq url-proxy-services '(("no_proxy" . "^\\(127.0.0.1\\|localhost\\|10.*\\)")))
 
 (provide 'init-python)
