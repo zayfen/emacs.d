8554e93cd617c3f2aec7dde0aca34f1c18441c75
zayfen <zhangyunfeng0101@gmail.com>
2019-03-18 19:33:23 +0800

    update packages

---
+++ elpa-26.1/company-php-20181110.303/company-php-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "company-php" "20181110.303" "company completion source for php" '((cl-lib "0.5") (ac-php-core "1") (company "0.9")) :commit "69a80eb21fcf8abceeafa488bb9e6d8b9793684b" :keywords '("completion" "convenience" "intellisense") :authors '((nil . "xcwenn@qq.com [https://github.com/xcwen]")) :maintainer '(nil . "xcwenn@qq.com [https://github.com/xcwen]") :url "https://github.com/xcwen/ac-php")
--- elpa-26.1/swiper-20180813.1625/swiper-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "swiper" "20180813.1625" "Isearch with an overview. Oh, man!" '((emacs "24.1") (ivy "0.9.0")) :commit "43474e59f915ab46b95446a379149e3af9c32226" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
